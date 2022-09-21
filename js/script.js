197b69fbbc3f606d9e265510f496af43fb8c9f78
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-21 10:07:24 +0200

    completed emails request from API

--- js/script.js
+++ js/script.js
@@ -3,6 +3,19 @@ console.log('Js Ok!');
 const app = new Vue({
     el: '#root',
     data: {
-
+        emails: []
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    console.log(response);
+                    const { data, status } = response;
+                    if (status == 200) {
+                        this.emails.push(data.response);
+                    }
+                });
+        }
     }
 });
