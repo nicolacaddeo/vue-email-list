7fdd3f5952de104588525aeb4651f0e19aba53f6
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-21 19:21:50 +0200

    added catch error

--- js/script.js
+++ js/script.js
@@ -15,7 +15,12 @@ const app = new Vue({
                     if (status == 200) {
                         this.emails.push(data.response);
                     }
-                });
+                })
+                .catch((error) => {
+                    console.log('Error');
+                })
+
+
         }
     }
 });
